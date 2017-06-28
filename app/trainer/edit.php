4eba1c631abc07f83ac42ca22bb311744e8b3f40
smith sookkhi <smith sookkhi>
2017-06-28 09:00:52 +0700

    Add trainer Option

--- app/trainer/edit.php
+++ app/trainer/edit.php
@@ -155,7 +155,7 @@ if ($_GET['trainer_id']) {
                                 $def = isset($trainer_method_assign) ? $trainer_method_assign : 'ผ่านการฝึกอบรม';
                                 //$sql = "SELECT trainer_property_id,trainer_property FROM trainer_property ORDER BY trainer_property_id ASC";
                                 $assign_data = array('T'=>'ผ่านการฝึกอบรม', 
-                                                'E'=>'มีประสบการณ์การสอนมากกว่า 6 เดือน');
+                                                'E'=>'มีประสบการณ์การสอนมากกว่า 6 เดือน','S' => 'ผ่านการทดสอบประเมินความรู้');
                                 echo gen_option($assign_data, $def);
                                 ?>
                             </select>
--- app/trainer/insert.php
+++ app/trainer/insert.php
@@ -141,7 +141,7 @@ if (isset($_POST['submit'])) {
                                             $def = isset($trainer_method_assign) ? $trainer_method_assign : 'ผ่านการฝึกอบรม';
                                             //$sql = "SELECT trainer_property_id,trainer_property FROM trainer_property ORDER BY trainer_property_id ASC";
                                             $assign_data = array('T' => 'ผ่านการฝึกอบรม',
-                                                'E' => 'มีประสบการณ์การสอนมากกว่า 6 เดือน');
+                                                'E' => 'มีประสบการณ์การสอนมากกว่า 6 เดือน','S' => 'ผ่านการทดสอบประเมินความรู้');
                                             echo gen_option($assign_data, $def);
                                             ?>
                                         </select>
